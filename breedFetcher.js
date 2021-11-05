78747c8473184564ae318df6479ea4528a89649a
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-11-04 20:13:59 -0400

    refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,26 +2,25 @@ const request = require('request');
 let breed = process.argv[2]
 let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`
 
-
+const fetchBreedDescription = function (breedname, callback) {
 request(url , (error , response, body)=> {
-const breed = body && JSON.parse(body)[0];
-
-
   if (error) {
-    console.log("error has occured")
-    console.log(error)
-  
-} else if(breed) { 
- console.log(breed.description) 
+    callback(error, null)
+    // console.log("error has occured")
+    // console.log(error)
+  }
+    const breed = body && JSON.parse(body)[0];
+  if(breed) { 
+ callback(null, breed.description) 
 } else  {
-console.log('breed doesnt exist')
+callback('breed doesnt exist')
 }
 
 // const data = JSON.parse(body);
 //     console.log(data[0].description)
 });  
-
-
+}
+module.exports =  {fetchBreedDescription }
 
 //console.log(typeof body)
 //console.log(data);
---
+++ index.js
@@ -0,0 +1,28 @@
+const  { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
+
+
+// request(url , (error , response, body)=> {
+//   const breed = body && JSON.parse(body)[0];
+  
+  
+//     if (error) {
+//       console.log("error has occured")
+//        console.log(error)
+    
+//   } else if(breed) { 
+//    console.log(breed.description) 
+//   } else  {
+//   console.log('breed doesnt exist')
+//   }
+// }
+
